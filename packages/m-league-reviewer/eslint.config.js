ddf9604015c3be9945193d59b54006882f58ef3a
sabertazimi <sabertazimi@gmail.com>
2026-04-13 19:21:12 +0800

    chore(eslint): ignore JSON assets in m-league-reviewer
    Exclude src/assets/**/*.json from ESLint to avoid linting
    data files like game-schedule.json.

--- packages/m-league-reviewer/eslint.config.js
+++ packages/m-league-reviewer/eslint.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from '@dg-scripts/eslint-config'
 
-export default defineConfig({
-  name: 'base',
-  rules: {
-    'react/no-nested-component-definitions': 'off',
-    'react-hooks/set-state-in-effect': 'off',
-    'react-hooks-extra/no-direct-set-state-in-use-effect': 'off',
-    'react-refresh/only-export-components': 'off',
-    'style/multiline-ternary': ['error', 'never'],
-    'ts/strict-boolean-expressions': 'off',
+export default defineConfig(
+  {
+    ignores: ['src/assets/**/*.json'],
   },
-})
+  {
+    name: 'base',
+    rules: {
+      'react/no-nested-component-definitions': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks-extra/no-direct-set-state-in-use-effect': 'off',
+      'react-refresh/only-export-components': 'off',
+      'style/multiline-ternary': ['error', 'never'],
+      'ts/strict-boolean-expressions': 'off',
+    },
+  },
+)
